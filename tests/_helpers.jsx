2bd8220445cc50f11d15d40395f95ab648cea861
Bruce Paul <bruce.paul@airbnb.com>
2017-10-05 11:44:46 -0700

    increase peerDep/devDep range for react and bump enzyme
    This pr updates react and related packages so that react16 is
    supported. Enzyme and related packages are also bumped to support
    react16. We also configure enzyme to work with whichever react is
    currently installed.

--- tests/_helpers.jsx
+++ tests/_helpers.jsx
@@ -3,6 +3,25 @@ import chai from 'chai';
 import sinonChai from 'sinon-chai';
 import sinon from 'sinon-sandbox';
 import chaiEnzyme from 'chai-enzyme';
+import Enzyme from 'enzyme';
+
+let Adapter;
+
+try {
+  Adapter = require('enzyme-adapter-react-16');
+} catch (e) {
+  try {
+    Adapter = require('enzyme-adapter-react-15');
+  } catch (e) {
+    try {
+      Adapter = require('enzyme-adapter-react-15.4');
+    } catch (e) {
+      Adapter = require('enzyme-adapter-react-14');
+    }
+  }
+}
+
+Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true });
 
 chai.use(sinonChai);
 chai.use(chaiEnzyme());
